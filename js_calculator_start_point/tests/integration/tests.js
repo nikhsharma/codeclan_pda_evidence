f53dcb2f15ea26978b071b934c3345e096e6d76f
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-06-24 23:12:05 +0100

    integration test passed: can chain multiple operations together

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -61,4 +61,15 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('725')
   })
 
+  it('should chain multiple operations together', function() {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number7')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('33')
+  })
+
 });
