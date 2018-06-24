2c2770d38f2008770a3c18d63ce8e928315f60d6
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-06-24 23:01:27 +0100

    integration test passed: can add 1+4

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -16,7 +16,7 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
-  it('should be able to multiply 3x5 to get 15', function(){
+  it('should be able to multiply 3x5 and get 15', function(){
     running_total = element(by.css('#running_total'))
     element(by.css('#number3')).click();
     element(by.css('#operator_multiply')).click();
@@ -25,7 +25,7 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('15')
   })
 
-  it('should be able to divide 21/7 to get 3', function() {
+  it('should be able to divide 21/7 and get 3', function() {
     running_total = element(by.css('#running_total'))
     element(by.css('#number2')).click();
     element(by.css('#number1')).click();
@@ -33,6 +33,15 @@ describe('calculator functionality', function() {
     element(by.css('#number7')).click();
     element(by.css('#operator_equals')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('3');
+  })
+
+  it('should add 1+4 and get 5', function() {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('5')
   })
 
 });
